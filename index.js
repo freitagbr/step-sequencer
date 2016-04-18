115cd1ae7466bc257986c9d3b99bb2f7fdc2e826
Brandon Freitag <freitagbr@gmail.com>
2016-04-17 22:55:11 -0700

    Use a prototype

--- index.js
+++ index.js
@@ -5,36 +5,40 @@ var inherits = require('util').inherits;
 var NanoTimer = require('nanotimer');
 
 function StepSequencer(tempo, division, sequence) {
-  var self = this;
-  var timer = new NanoTimer();
-  var timeout = Math.floor((60 / (tempo * division)) * 10e5) + 'u';
-
-  self.tempo = tempo;
-  self.division = division;
-  self.sequence = sequence;
-  self.step = 0;
-
-  function advance() {
-    self.emit('' + self.step, self.sequence[self.step]);
-    self.step = (self.step + 1) ;
-    if (self.step === self.sequence.length) self.step = 0;
-  }
-
-  self.play = function () {
-    self.step = 0;
-    timer.setInterval(advance, '', timeout);
-  };
-
-  self.resume = function () {
-    timer.setInterval(advance, '', timeout);
-  };
-
-  self.stop = function () {
-    timer.clearInterval();
-  };
-
-  EventEmitter.call(self);
+  this.tempo = tempo;
+  this.division = division;
+  this.sequence = sequence;
+  this.step = 0;
+  this.timer = new NanoTimer();
+  this.timeout = Math.floor((60 / (tempo * division)) * 10e8) + 'n';
+  EventEmitter.call(this);
 }
 
 inherits(StepSequencer, EventEmitter);
+
+StepSequencer.prototype._advance = function () {
+  this.emit('' + this.step, this.sequence[this.step]);
+  this.step = (this.step + 1);
+  if (this.step === this.sequence.length) this.step = 0;
+}
+
+StepSequencer.prototype.play = function () {
+  var self = this;
+  self.step = 0;
+  self.timer.setInterval(function () {
+    self._advance.call(self);
+  }, '', self.timeout);
+};
+
+StepSequencer.prototype.resume = function () {
+  var self = this;
+  self.timer.setInterval(function () {
+    self._advance.call(self);
+  }, '', self.timeout);
+};
+
+StepSequencer.prototype.stop = function () {
+  this.timer.clearInterval();
+};
+
 module.exports = StepSequencer;
